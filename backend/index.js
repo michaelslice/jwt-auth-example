2a22df71e5e6a176d9d33105a033810d833bf959
michaelslice <mjsens523@gmail.com>
2025-02-25 12:41:26 -0600

    feat: Added frontend and backend code

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
-import { createServer } from 'node:http';
+const http = require('node:http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = createServer((req, res) => {
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  
+  if(req.url === "/api/jwt" && req.method === "POST"){
+    console.log("TEST")
+    res.writeHead(200)
+    return res.end(JSON.stringify({"Message": "FSAFAS"}))
+  }
   res.end('Hello World');
 });
 
